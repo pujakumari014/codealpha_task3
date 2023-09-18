915cdda0a97ac3eb5950d78d976feea972fef952
Puja Kumari <143801852+pujakumari014@users.noreply.github.com>
2023-09-18 20:44:32 +0530

    Add files via upload

---
+++ Landing Page/index.js
@@ -0,0 +1,7 @@
+function openNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "50%";
+}
+function closeNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "0%";
